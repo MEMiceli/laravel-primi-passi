07a216ca700e1483e59e743f6dc4fe696a21eb2a
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-07-06 16:04:30 +0200

    done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Esperimento</title>
+</head>
+<body>
+    <h1>
+        Esperimento
+    </h1>
+    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nihil! Cumque temporibus optio, fugit veniam nam quidem sint commodi tempora autem dolore omnis nihil illum alias non earum aperiam sit.</h4>
+</body>
+</html>
